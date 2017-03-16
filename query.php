3e293a3b0245a49536dc7253fb7d6dd0b4e08fb5
HrafnkellTorri <Kallinnhannhrabbi@hotmail.com>
2017-03-16 14:44:41 +0000

    Delete query.php

--- query.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once 'connection.php';
-$sql = 'SELECT * FROM myndir ORDER by myndir.myndheiti';
-$result = $pdo->query($sql);
-$numrows = $result->rowcount();
-echo "<P> Fundið var $numrows eintök.</P>";
-?>
